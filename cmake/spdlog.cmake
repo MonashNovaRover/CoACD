12fde193883316c1350f8463f373d24b31128ccd
Victor Bartlinski <vicbart08@gmail.com>
2025-08-13 17:41:47 +1000

    fix?

--- cmake/spdlog.cmake
+++
@@ -1,9 +0,0 @@
-find_package(spdlog REQUIRED)
-
-set(CMAKE_INSTALL_DEFAULT_COMPONENT_NAME spdlog)
-if(NOT spdlog_POPULATED)
-  FetchContent_Populate(spdlog)
-  add_subdirectory(${spdlog_SOURCE_DIR} ${spdlog_BINARY_DIR} EXCLUDE_FROM_ALL)
-endif()
-
-set_target_properties(spdlog PROPERTIES POSITION_INDEPENDENT_CODE ON)
--- cmake/zlib.cmake
+++
@@ -1,7 +0,0 @@
-find_package(ZLIB REQUIRED)
-
-set_target_properties(ZLIB::ZLIB PROPERTIES POSITION_INDEPENDENT_CODE TRUE)
-set(ZLIB_LIBRARY ZLIB::ZLIB)
-set(ZLIB_INCLUDE_DIR ${zlib_SOURCE_DIR})
-set(ZLIB_FOUND TRUE)
-target_include_directories(ZLIB::ZLIB PUBLIC ${zlib_SOURCE_DIR} ${zlib_BINARY_DIR})
