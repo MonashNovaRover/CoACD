20cc4134ae466a265556ffc1658be93da48f8cc3
Victor Bartlinski <vicbart08@gmail.com>
2025-08-13 17:29:35 +1000

    fixing boost?

--- cmake/boost.cmake
+++ cmake/boost.cmake
@@ -3,7 +3,7 @@ set(BOOST_IOSTREAMS_ENABLE_ZSTD OFF CACHE BOOL "" FORCE)
 set(BOOST_IOSTREAMS_ENABLE_LZMA OFF)
 set(BOOST_IOSTREAMS_ENABLE_BZIP2 OFF)
 
-find_package(BOOST REQUIRED)
+find_package(Boost REQUIRED)
 
 if(zlib_SOURCE_DIR)
     target_include_directories(boost_iostreams PRIVATE ${zlib_SOURCE_DIR} ${zlib_BINARY_DIR})
